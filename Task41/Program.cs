b8a9c1b42a77060bcef50fc4e9dca3d3dc6e34b0
Andrey Perevertov <gociioduh@gmail.com>
2023-03-04 23:09:26 +0300

    Seminar6

---
+++ Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] InputArray(string text)
+{
+    System.Console.WriteLine(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+int NumberPositive(int[] array)
+{
+    int quantity = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) quantity++;
+    }
+    return quantity;
+}
+Console.WriteLine($"Введено {NumberPositive(InputArray("Введите числа через пробел:"))} положительных чисел");
---
+++ Task43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+int[] InputArray(string text)
+{
+    Console.WriteLine(text);
+    int[] array = new int[4];
+    string[] arrayText = new string[4] { "b1", "k1", "b2", "k2" };
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите значение {arrayText[i]}:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+int[] FindingTheIntersectionPoint(int[] array)
+{
+    int[] arrayXY = new int[2];
+    if (array[1] == array[3] && array[0] == array[2])
+    {
+        Console.WriteLine("Графики совпадают");
+    }
+    else if (array[1] == array[3])
+    {
+        Console.WriteLine("Графики параллельны");
+    }
+    else
+    {
+        arrayXY[0] = (array[2] - array[0]) / (array[1] - array[3]);
+        arrayXY[1] = array[1] * arrayXY[0] + array[0];
+    }
+    return arrayXY;
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine("(" + string.Join("; ", array) + ")");
+}
+
+PrintArray(FindingTheIntersectionPoint(InputArray("Для определения точки пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2")));
+
